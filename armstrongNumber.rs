d8df63a676b9f4e83be04555ea663f0c6e638f93
AlphaCoder007-GCI <58473430+AlphaCoder007-GCI@users.noreply.github.com>
2019-12-04 21:29:07 +0530

    Update armstrongNumber.rs

--- armstrongNumber.rs
+++ armstrongNumber.rs
@@ -5,12 +5,17 @@ pub fn is_armstrong_number(num: u32) -> bool {
 }
 
 fn main() {
-    let my_num = 15;
-    let ans = is_armstrong_number(my_num);
     
-    if ans == true {
-        println!("The given number {} is an Armstrong Number.", my_num);
-    } else {
-        println!("The given number {} is not an Armstrong Number.", my_num);
+    println!("It's the list of all Armstrong numbers between 0 and 1000.");
+
+    for my_num in 0..1000 {
+        // let my_num = 15;
+        let ans = is_armstrong_number(my_num);
+        
+        if ans == true {
+            println!("The given number {} is an Armstrong Number.", my_num);
+        // } else {
+        //     println!("The given number {} is not an Armstrong Number.", my_num);
+        }
     }
 }
