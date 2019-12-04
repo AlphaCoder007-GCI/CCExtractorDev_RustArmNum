cf29769cbeebfc0e1695a95052b3b5173620bd0e
AlphaCoder007-GCI <58473430+AlphaCoder007-GCI@users.noreply.github.com>
2019-12-04 21:22:59 +0530

    Update armstrongNumber.rs

--- armstrongNumber.rs
+++ armstrongNumber.rs
@@ -3,3 +3,14 @@ pub fn is_armstrong_number(num: u32) -> bool {
     let len = s.len() as u32;
     s.chars().map(|c| (c as u32 - '0' as u32).pow(len)).sum::<u32>() == num
 }
+
+fn main() {
+    let my_num = 15;
+    let ans = is_armstrong_number(my_num);
+    
+    if ans == true {
+        println!("The given number {} is an Armstrong Number.", my_num);
+    } else {
+        println!("The given number {} is not an Armstrong Number.", my_num);
+    }
+}
